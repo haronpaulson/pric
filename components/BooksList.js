3c9d44c86b376011fc37ea43d932a320fed5a46c
Your Name <haronpaulson@gmail.com>
2022-11-17 14:53:47 +0530

    imag

--- components/BooksList.js
+++ components/BooksList.js
@@ -27,6 +27,7 @@ function BooksList() {
       
       {movies.map(function(movie) {
         return <div className='mt-8 flex space-x-4 border-b-2 pb-3 '>
+      
         <Image src={prefix + movie.picture} alt="bookss" width="170" height="70" objectFit='contain' />
         <div>
             <p1 > {movie.name}</p1><br/>

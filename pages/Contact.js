2d28708287ca712ba9784817cf066b2824e3d2d9
Your Name <haronpaulson@gmail.com>
2022-12-07 15:22:27 +0530

    add books

--- pages/Contact.js
+++ pages/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Contact() {
   return (
     <div className=' bg-slate-500'>   
-        <h1>welcome to our page</h1>
+        <h1>welcome to contacts</h1>
     </div>
   )
 }
